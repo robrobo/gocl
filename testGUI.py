85c0fc892d0a12371a870e36ba6d2ef127099852
pbonack <Patrick.Bonack@stud.uni-frankfurt.de>
2016-09-10 15:58:45 +0200

    gui update

--- testGUI.py
+++ testGUI.py
@@ -20,19 +20,30 @@ from PyQt5 import uic
 
 class Example(QMainWindow):
     
-    def __init__(self):
+    def __init__(self, pics):
         super().__init__()
-        uic.loadUi('simpleGUI.ui', self)        
-
+        uic.loadUi('simpleGUI.ui', self)    
+        self.scrollAreaWidgetContents = QLabel()    
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.spinBox.setMinimum(0)
+        self.spinBox.setMaximum(len(pics)-1)  
+        self.pics = pics
+        self.pixmaps =[QPixmap(a) for a in self.pics] 
+        self.spinBox.valueChanged.connect(self.spinChange)
+        self.spinBox.setValue(0)
+        self.spinChange()
         
+    def spinChange(self):
+        i = self.spinBox.value()
+        self.scrollAreaWidgetContents.setPixmap(self.pixmaps[i]) 
                 
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
-    ex = Example()
-    ex.scrollAreaWidgetContents = QLabel()
-    pixmap = QPixmap("Rabbit-1.jpg")
-    ex.scrollAreaWidgetContents.setPixmap(pixmap) 
-    ex.scrollArea.setWidget(ex.scrollAreaWidgetContents)    
+    pics = ["rabbit-01.jpg","rabbit-02.jpg","rabbit-03.jpg","rabbit-04.jpg","rabbit-05.jpg","rabbit-06.jpg"]
+    ex = Example(pics)
+    
+    
+
     ex.show()
     sys.exit(app.exec_())  
